bb916f9c9fd26a61fc97cd961ebe48e7d127d38c
Dawid Klos <klosdawid12@gmail.com>
2023-03-27 18:19:23 +0100

    feat: setting up engine precision in one file

---
+++ include/engine/precision.h
@@ -0,0 +1,36 @@
+//
+// Created by dave on 27.03.23.
+//
+
+#ifndef SIMPLE_PHYSICS_ENGINE_PRECISION_H
+#define SIMPLE_PHYSICS_ENGINE_PRECISION_H
+
+#include <cfloat>
+
+/**
+ * Code adapted from Millington, 2010
+ */
+
+namespace engine {
+    /**
+     * Defines a real number precision. Allows to simply change the precision if needed.
+     * By default single-precision is used.
+     */
+    typedef  float real;
+
+    /**
+     * Defines the precision of the square root operator.
+     */
+    #define real_sqrt sqrt
+
+    /**
+     * Defines the precision of the power operator.
+     */
+    #define real_pow powf
+}
+
+/**
+ * End of adapted code
+ */
+
+#endif //SIMPLE_PHYSICS_ENGINE_PRECISION_H
